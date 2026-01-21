3f34286790f576112eb0abb6561d768d88cd93eb
Eric Chuang <ericchuang94@gmail.com>
2026-01-21 14:19:40 -0800

    simplify

--- keyboards/sofle/keymaps/ericbreh/config.h
+++ keyboards/sofle/keymaps/ericbreh/config.h
@@ -2,7 +2,3 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-
-#define TRI_LAYER_LOWER_LAYER 1
-#define TRI_LAYER_UPPER_LAYER 2
-#define TRI_LAYER_ADJUST_LAYER 3
--- keyboards/sofle/keymaps/ericbreh/keymap.c
+++ keyboards/sofle/keymaps/ericbreh/keymap.c
@@ -4,15 +4,11 @@
 
 enum sofle_layers {
     _QWERTY,
-    _LOWER,
     _RAISE,
-    _ADJUST,
 };
 
 enum custom_keycodes {
-    KC_PRVWD = QK_USER,
-    KC_NXTWD,
-    KC_LSTRT,
+    KC_LSTRT = QK_USER,
     KC_LEND
 };
 
@@ -23,74 +19,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,  KC_W,     KC_E,     KC_R,     KC_T,                         KC_Y,     KC_U,     KC_I,     KC_O,    KC_P,     KC_BSLS,
   KC_ESC,   KC_A,  KC_S,     KC_D,     KC_F,     KC_G,                         KC_H,     KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,  KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  XXXXXXX,  KC_N,     KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_ENT,
-                   KC_SPC,   KC_LALT,  TL_LOWR,  KC_LCTL,  KC_LGUI,  LT(_RAISE, KC_SPC), KC_RSFT,  KC_RCTL,  KC_RGUI, KC_RALT
-),
-
-[_LOWER] = LAYOUT(
-  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
-  _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_F12,
-  _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,                      KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_PIPE,
-  _______,  KC_EQL,   KC_MINS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_SCLN,  KC_COLN,  KC_BSLS,  _______,
-                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+                   XXXXXXX,  XXXXXXX,  KC_LALT,  KC_LCTL,  KC_LGUI,  LT(_RAISE, KC_SPC), KC_RSFT,  KC_RCTL,  XXXXXXX, XXXXXXX
 ),
 
 [_RAISE] = LAYOUT(
-  _______,  _______,  _______,  _______,  _______,  _______,                      KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_BRID,  KC_BRIU,  KC_DEL,
-  _______,  KC_INS,   KC_PSCR,  KC_APP,   XXXXXXX,  XXXXXXX,                      KC_PGUP,  KC_MINS,   KC_EQL,   KC_LBRC,  KC_RBRC,  XXXXXXX,
-  _______,  KC_LALT,  KC_LCTL,  KC_LSFT,  XXXXXXX,  KC_CAPS,                      KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,
-  _______,  C(KC_Z),  C(KC_X),  C(KC_C),  C(KC_V),  XXXXXXX,  _______,  _______,  KC_PGDN,  KC_LSTRT,  XXXXXXX,  KC_LEND,  XXXXXXX,  _______,
+  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,                        KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_BRID,  KC_BRIU,  KC_DEL,
+  KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                       KC_PGUP,  KC_MINS,   KC_EQL,   KC_LBRC,  KC_RBRC,  XXXXXXX,
+  XXXXXXX,  XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_CAPS,                      KC_LEFT,  KC_DOWN,   KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,
+  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSCR,  _______,  _______,  KC_PGDN,  KC_LSTRT,  XXXXXXX,  KC_LEND,  XXXXXXX,  _______,
                       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______
-),
-
-[_ADJUST] = LAYOUT(
-  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-  QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  CG_TOGG,  XXXXXXX,                      XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-  XXXXXXX,  XXXXXXX,  CG_TOGG,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      XXXXXXX,  KC_VOLD,  KC_MUTE,  KC_VOLU,  XXXXXXX,  XXXXXXX,
-  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  XXXXXXX,  KC_MPRV,  KC_MPLY,  KC_MNXT,  XXXXXXX,  XXXXXXX,
-                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-  )
+)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case KC_PRVWD:
-            if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    register_mods(mod_config(MOD_LALT));
-                    register_code(KC_LEFT);
-                } else {
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_LEFT);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LALT));
-                    unregister_code(KC_LEFT);
-                } else {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_LEFT);
-                }
-            }
-            break;
-        case KC_NXTWD:
-             if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    register_mods(mod_config(MOD_LALT));
-                    register_code(KC_RIGHT);
-                } else {
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_RIGHT);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LALT));
-                    unregister_code(KC_RIGHT);
-                } else {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_RIGHT);
-                }
-            }
-            break;
         case KC_LSTRT:
             if (record->event.pressed) {
                 if (keymap_config.swap_lctl_lgui) {
